b3551adbacd621a7cd1187c8178986154bc4afb9
Burbinox <hubert.grobelny@interia.pl>
2021-12-10 18:36:56 +0100

    adjust getElement method

--- src/js/DOM_manipulation.js
+++ src/js/DOM_manipulation.js
@@ -1,7 +1,6 @@
 export default class DomManipulation{
     getElement(elem_id){
         let elem = document.getElementById(elem_id);
-        let elem_value = elem.innerText
-        return [elem, elem_value]
+        return elem
     }
 }
--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,6 @@ import './module2';
 import DomManipulation from './DOM_manipulation';
 
 console.log('Hello world!');
-test = new DomManipulation()
-console.log(test.getElement("test"))
+dom_obj = new DomManipulation()
+console.log(dom_obj.getElement("test"))
 
