61b8450d86ed58c63d9ebb20c3f705ac0bb7c30b
patrykbrodziak <patryk.brodziak1@gmail.com>
2021-12-12 14:21:57 +0100

    add coments

--- src/js/api-class.js
+++ src/js/api-class.js
@@ -1,5 +1,11 @@
 const URL_BASE = 'https://metaweather-api.glitch.me/';
 
+ /**
+ *  The fetch method that sends queries, returns data after receiving a response. On failure, an error is returned.
+ *  
+ * @param {String} urlEndpoint is a expansion of the underlying query "URL_BASE"
+ * @return {JSON} json object with info
+ */
 const fetchData = async (urlEndpoint) => {
   try {
     const response = await fetch(`${URL_BASE}${urlEndpoint}`);
@@ -17,6 +23,13 @@ const fetchData = async (urlEndpoint) => {
 };
 
 export default class WeatherAPI {
+
+  /**
+  *  The method queries the API through query and returns data such as: city, id, longitude and latitude
+  *  
+  * @param {String} name means query in the form of a city name
+  * @return {JSON} json object with info: city, id, longitude and latitude 
+  */
   async getLocationId(name) {
     const apiEndpoint = `/api/location/search/?query=${name}`;
 
@@ -25,6 +38,12 @@ export default class WeatherAPI {
     return data;
   }
 
+  /**
+  *  The method queries the API through locationID and returns info about the weather in real time
+  *  
+  * @param {Number} id means the specific locationID
+  * @return {JSON} json object with info about the weather
+  */
   async getWeatherData(id) {
     const apiEndpoint = `/api/location/${id}`;
 
@@ -35,7 +54,13 @@ export default class WeatherAPI {
 
     return data;
   }
-
+  /**
+  *  The method queries the API through locationID and data returns info about the historical weather 
+  *  
+  * @param {Number} id means the specific locationID
+  * @param {String} date in format 'YYYY/MM/DD'
+  * @return {JSON} json object with info about the historical weather
+  */
   async getHistoricalWeatherData(id, date) {
     const apiEndpoint = `/api/location/${id}/${date}`;
 
