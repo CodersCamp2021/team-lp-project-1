aa8d47cc30eaa442640d3a8fc57ed38d2e01d768
pastepi <pawel.st123@gmail.com>
2021-12-12 21:31:44 +0100

    Added comments to class methods

--- src/js/DomManipulation.js
+++ src/js/DomManipulation.js
@@ -1,4 +1,9 @@
 export default class DomManipulation {
+  /**
+   * Sets 'this.elem' to an element with corresponding ID
+   * of the instantiated object.
+   * @param {string} elemID
+   */
   constructor(elemID) {
     this.elem = document.getElementById(elemID);
   }
@@ -11,10 +16,19 @@ export default class DomManipulation {
     this.elem.src = imgPath;
   }
 
+  /**
+   * Transforms the wind direction icon to
+   * reflect the wind direction data.
+   * @param {Number} direction
+   */
   setWindIcon(direction) {
     this.elem.style.transform = `rotate(${direction}deg)`;
   }
 
+  /**
+   * Method responsible for hiding/showing elements,
+   * will be used to swap layouts.
+   */
   setDisplayToggle() {
     this.elem.style.display =
       this.elem.style.display === 'none' ? 'block' : 'none';
