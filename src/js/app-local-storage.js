889d58a1a96d72b41c0cc9ffdcba61e2ac8297cd
patrykbrodziak <patryk.brodziak1@gmail.com>
2021-12-13 19:25:36 +0100

    Add comments to app-local-storage class

--- src/js/app-local-storage.js
+++ src/js/app-local-storage.js
@@ -1,4 +1,10 @@
 export default class AppLocalStorage {
+  /**
+  *  Method that allows to add an element to the localStorage container
+  *  
+  * @param {String} key is a key of the value we want to add
+  * @param {String} value is a value we want to add
+  */
   set(key, value) {
     try {
       localStorage.setItem(key, value);
@@ -7,6 +13,12 @@ export default class AppLocalStorage {
     }
   }
 
+  /**
+  *  Method that allows to get an element from the localStorage container
+  *  
+  * @param {String} key is a key of the value we want to get
+  * @return {String} the current value associated with the given key, or null if the given key does not exist.
+  */
   get(key) {
     try {
       return localStorage.getItem(key);
@@ -16,6 +28,11 @@ export default class AppLocalStorage {
     return null;
   }
 
+  /**
+  *  Method that allows to remove an element from the localStorage container
+  *  
+  * @param {String} key is a key of the value we want to remove
+  */
   remove(key) {
     try {
       localStorage.removeItem(key);
