fbb19e7c8c5f3213c35ab4368416b01da9e577f2
pastepi <pawel.st123@gmail.com>
2021-12-15 22:12:15 +0100

    Added comment with source to 'debounce' func, slight tweak to handleInput

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,16 @@ const dataList = new DomManipulation('results');
 
 searchView.toggleDisplay();
 
+/**
+ * Source of the debounce function:
+ * https://levelup.gitconnected.com/debounce-in-javascript-improve-your-applications-performance-5b01855e086
+ * Debounce allows the app to wait for the user to pause typing
+ * before requesting data from API. This way the requests won't be sent out
+ * every keystroke. *
+ * @param {function} func function to be invoked after the delay specified with 'wait'.
+ * @param {number} wait time in ms
+ * @returns {function} closure
+ */
 const debounce = (func, wait) => {
   let timeout;
 
@@ -29,7 +39,7 @@ const debounce = (func, wait) => {
 };
 
 const handleInput = async (e) => {
-  if (e.target.value) {
+  if (e.target.value && e.key !== 'Enter') {
     const res = await weather.getQueryLocations(e.target.value);
     console.log(res);
     dataList.setDatalistChildren(res);
