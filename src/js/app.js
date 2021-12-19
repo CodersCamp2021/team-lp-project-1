71211780c8d4e3b72cf6ea34a5b30b9c7a997845
patrykbrodziak <patryk.brodziak1@gmail.com>
2021-12-19 21:18:23 +0100

    add daily update object

--- src/js/app.js
+++ src/js/app.js
@@ -8,36 +8,36 @@ const weather = new WeatherAPI();
 const searchInput = new DomManipulation('home-input');
 const homeView = new DomManipulation('home-view');
 const searchView = new DomManipulation('search-view');
-const dataList = new DomManipulation('results');
 
-const dupa = async(data) => {
-    const dailyCityName = new DomManipulation('daily-city-name')
-    dailyCityName.setText(data.title)
-    const dailyCurrentTime = new DomManipulation('daily-current-time')
-    dailyCurrentTime.setText(data.time.substr(11,5))
+const setWeatherInfo = async(data) => {
+    const dailyCityName = new DomManipulation('daily-city-name');
+    dailyCityName.setText(data.title);
+    const dailyCurrentTime = new DomManipulation('daily-current-time');
+    dailyCurrentTime.setText(data.time.substr(11,5));
     const abbr = new DomManipulation('daily-abbr');
-    abbr.setImage(`https://www.metaweather.com/static/img/weather/${data.consolidated_weather[0].weather_state_abbr}.svg`)
-    const dailyTemp = new DomManipulation('daily-temp')
-    dailyTemp.setText(`${parseInt(data.consolidated_weather[0].the_temp, 10)}°C`)
-    const dailyState = new DomManipulation('daily-state')
-    dailyState.setText(data.consolidated_weather[0].weather_state_name)
-    const dailyMin = new DomManipulation('daily-min')
-    dailyMin.setText(`min: ${parseInt(data.consolidated_weather[0].min_temp, 10)}°C`)
-    const dailyMax = new DomManipulation('daily-max')
-    dailyMax.setText(`max: ${parseInt(data.consolidated_weather[0].max_temp, 10)}°C`)
-    const dailyArrow = new DomManipulation('daily-arrow')
-    dailyArrow.setWindIcon(data.consolidated_weather[0].wind_direction)
-    const dailyWindSpeed = new DomManipulation('daily-wind-speed')
+    abbr.setImage(`https://www.metaweather.com/static/img/weather/${data.consolidated_weather[0].weather_state_abbr}.svg`);
+    const dailyTemp = new DomManipulation('daily-temp');
+    dailyTemp.setText(`${parseInt(data.consolidated_weather[0].the_temp, 10)}°C`);
+    const dailyState = new DomManipulation('daily-state');
+    dailyState.setText(data.consolidated_weather[0].weather_state_name);
+    const dailyMin = new DomManipulation('daily-min');
+    dailyMin.setText(`min: ${parseInt(data.consolidated_weather[0].min_temp, 10)}°C`);
+    const dailyMax = new DomManipulation('daily-max');
+    dailyMax.setText(`max: ${parseInt(data.consolidated_weather[0].max_temp, 10)}°C`);
+    const dailyArrow = new DomManipulation('daily-arrow');
+    dailyArrow.setWindIcon(data.consolidated_weather[0].wind_direction);
+    const dailyWindSpeed = new DomManipulation('daily-wind-speed');
     dailyWindSpeed.setText(`${parseInt(data.consolidated_weather[0].wind_speed, 10)}\n mph`);
+    const lastUpdate = new DomManipulation('daily-update');
+    setUpdatedTime(lastUpdate, data.consolidated_weather[0].created)
     for (let day = 1; day < 6; day++){
-        console.log(`card${day}-day`);
         const abbr = new DomManipulation(`card${day}-abbr`);
         const dailyTemp = new DomManipulation(`card${day}-temp`);
         const dailyState = new DomManipulation(`card${day}-state`);
         const dailyWindSpeed = new DomManipulation(`card${day}-wind`);
         const dailyArrow = new DomManipulation(`card${day}-arrow`);
-        abbr.setImage(`https://www.metaweather.com/static/img/weather/${data.consolidated_weather[day].weather_state_abbr}.svg`)
-        dailyTemp.setText(`${parseInt(data.consolidated_weather[day].the_temp, 10)}°C`)
+        abbr.setImage(`https://www.metaweather.com/static/img/weather/${data.consolidated_weather[day].weather_state_abbr}.svg`);
+        dailyTemp.setText(`${parseInt(data.consolidated_weather[day].the_temp, 10)}°C`);
         dailyState.setText(data.consolidated_weather[day].weather_state_name);
         dailyWindSpeed.setText(`${parseInt(data.consolidated_weather[day].wind_speed, 10)}\n mph`);
         dailyArrow.setWindIcon(data.consolidated_weather[day].wind_direction);
@@ -49,7 +49,7 @@ const getDataFromAPI = async (e) => {
     const woeid = res[0].woeid
     const data = await weather.getWeatherData(woeid)
     homeView.setDisplayToggle()
-    await dupa(data);
+    await setWeatherInfo(data);
     searchView.setDisplayToggle()
 }
 
@@ -58,3 +58,17 @@ searchInput.elem.addEventListener('keyup', event => {
         const data = getDataFromAPI(event)
     }
 });
+
+function setUpdatedTime(dailyUpdateObject, createdTime) {
+    const timeNow = Date.now();
+    const infoCreatedTime = Date.parse(createdTime);
+    const duration = timeNow - infoCreatedTime;
+    let seconds = Math.floor((duration / 1000) % 60),
+      minutes = Math.floor((duration / (1000 * 60)) % 60),
+      hours = Math.floor((duration / (1000 * 60 * 60)) % 24);
+    hours = (hours < 10) ? "0" + hours : hours;
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+    seconds = (seconds < 10) ? "0" + seconds : seconds;
+    dailyUpdateObject.setText(`Updated ${hours} hours ${minutes} minutes ago`);
+  };
+
