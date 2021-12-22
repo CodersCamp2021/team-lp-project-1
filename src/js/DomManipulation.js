f630d72533fc183acec4ae96e06d4d5cd5138dc8
Maciek <maciek.duzy.11@gmail.com>
2021-12-22 14:02:41 +0100

    removed unused methods

--- src/js/DomManipulation.js
+++ src/js/DomManipulation.js
@@ -49,12 +49,4 @@ export default class DomManipulation {
     this.elem.style.display =
       this.elem.style.display === 'none' ? 'block' : 'none';
   }
-
-  makeActive() {
-    this.elem.classList.add('active');
-  }
-
-  makeNotActive() {
-    this.elem.classList.remove('active');
-  }
 }
