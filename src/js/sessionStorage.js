4cee450036238cfeb29770bc2fc1218aa462b2fe
Jakub Czerwinski <kubaczerwinski77@gmail.com>
2021-12-28 13:59:34 +0100

    Added session storage class

---
+++ src/js/sessionStorage.js
@@ -0,0 +1,21 @@
+class SessionStorage {
+  /**
+   * The function sets the data in SessionStorage
+   * depends on the key and value pair
+   * @param {String} key
+   * @param {String} value
+   */
+  static set(key, value) {
+    window.sessionStorage.setItem(key, value);
+  }
+
+  /**
+   * If the key is defined in Session Storage, function
+   * returns the value, otherwise it returns null
+   * @param {String} key
+   * @returns {String}
+   */
+  static get(key) {
+    return window.sessionStorage.getItem(key);
+  }
+}
